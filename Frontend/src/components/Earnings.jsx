35c1b1478428ca2dea9ac180db533fe4548ec59b
rahuldhanak11 <rahuldhanak11@gmail.com>
2024-10-20 01:54:54 +0530

    titles

--- Frontend/src/components/Earnings.jsx
+++ Frontend/src/components/Earnings.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 function Earnings() {
   return (
-    <div>
-    <div className='bg-[#11111] w-full h-full p-10 rounded-xl'>
-      <h2 className='text-[#FFCC00] text-7xl font-pricedown'>MoneY</h2>
-    </div>
-  
+    <div className='bg-[#11111] w-full h-full p-10 rounded-xl flex'>
+      <div className='flex flex-col'>
+    <div className='relative'>
+      <h2 className='text-[#FFCC00] text-6xl font-pricedown'>MoneY</h2>
+      <p className='text-white font-signPainter absolute left-36 -bottom-6 text-2xl'>Hesoyam</p>
+      </div>
+      
+      </div>
     </div>
   )
 }
--- Frontend/src/components/PerformanceCard.jsx
+++ Frontend/src/components/PerformanceCard.jsx
@@ -4,6 +4,7 @@ function PerformanceCard() {
   return (
     <div className='bg-[#F55600] w-full h-full p-10 rounded-xl'>
       <h2 className='text-[#FFCC00] bg-[#F55600] text-5xl text-center font-pricedown'>Overall Performance</h2>
+      <h1 className='text-center flex justify-center mt-12 font-pricedown text-white text-9xl'>96</h1>
     </div>
   )
 }
