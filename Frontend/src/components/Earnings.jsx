2c8007523da00f4936efd6c377fbae4e51cbad50
rahuldhanak11 <rahuldhanak11@gmail.com>
2024-10-20 01:29:02 +0530

    salescard

--- Frontend/src/components/Earnings.jsx
+++ Frontend/src/components/Earnings.jsx
@@ -4,7 +4,7 @@ function Earnings() {
   return (
     <div>
     <div className='bg-[#11111] w-full h-full p-10 rounded-xl'>
-      <h2 className='text-[#FFCC00] text-7xl font-pricedown'>Money</h2>
+      <h2 className='text-[#FFCC00] text-7xl font-pricedown'>MoneY</h2>
     </div>
   
     </div>
--- Frontend/src/components/SalesCard.jsx
+++ Frontend/src/components/SalesCard.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 
-function SalesCard() {
+function SalesCard({ data }) {
   return (
-    <div className='bg-[#11111] w-full h-full p-10 rounded-xl'>
+    <div className='bg-[#11111] w-full h-full p-10 rounded-xl flex'>
+      <div className='w-[200px] flex flex-col'>
+      <div className='relative '>
       <h2 className='text-[#FFCC00] text-6xl font-pricedown'>Sales</h2>
-    </div>
+      <p className='text-white font-signPainter absolute left-28 -bottom-4'>Here We Go</p>
+      </div>
+      <div className='flex flex-col items-start mt-10 gap-2'>
+        <button className='font-chaletLondon bg-[#FFCC00] w-full py-1 rounded-xl'>Weekly</button>
+        <button className='font-chaletLondon bg-[#FFCC00] w-full py-1 rounded-xl'>Monthly</button>
+        <button className='font-chaletLondon bg-[#FFCC00] w-full py-1 rounded-xl'>Yearly</button>
+      </div>
+      <div className='flex flex-col items-start mt-4 gap-2'>
+        <button className='font-chaletLondon text-white border-gray-100 border w-full py-1 rounded-xl'>Hindu</button>
+        <button className='font-chaletLondon text-white border-gray-100 border w-full py-1 rounded-xl'>Muslim</button>
+        <button className='font-chaletLondon text-white border-gray-100 border w-full py-1 rounded-xl'>Jain</button>
+        <button className='font-chaletLondon text-white border-gray-100 border w-full py-1 rounded-xl'>Catholic</button>
+      </div>
+      </div> 
+      <div>
+        {/* charts */}
+        </div>   
+      </div>
   )
 }
 
