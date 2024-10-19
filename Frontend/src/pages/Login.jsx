7d03a5c06ad70cbcbecb5665cb12cec23b216f25
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2024-10-20 00:00:33 +0530

    Updated colors

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -5,16 +5,16 @@ function Login() {
     <div className="bg-[#FFCC00] w-screen flex justify-center items-center overflow-hidden">
         <div className='h-screen w-full max-w-[1440px] bg-[#FFCC00] flex'>
             <div className="flex-1 flex justify-center items-center">
-                <div className="w-[500px] h-[500px] bg-[#FF6A00] rounded-3xl flex flex-col justify-center items-center relative">
+                <div className="w-[500px] h-[500px] bg-[#F55600] rounded-3xl flex flex-col justify-center items-center relative">
                     <p className="font-signPainter text-5xl text-white">Login</p>
                     <form action="" className="flex flex-col">
                         <p className="text-white font-chaletLondon text-lg">UserID</p>
-                        <input type="text" placeholder="Enter your UserID" className="w-[300px] h-[40px] bg-[#ffbb00d2] rounded-2xl p-2 placeholder:text-[#ffffffcb]" />
+                        <input type="text" placeholder="Enter your UserID" className="w-[300px] h-[40px] outline-none text-white font-chaletLondon placeholder:font-chaletLondon bg-[#ffbb00d2] rounded-2xl p-2 placeholder:text-[#ffffffcb]" />
                         <p className="text-white font-chaletLondon text-lg mt-3">Password</p>
                         <input type="password" placeholder="Password" className="w-[300px] h-[40px] bg-[#ffbb00d2] rounded-2xl p-2 placeholder:text-[#ffffffcb]" />
                         <button type="submit" className="w-[300px] h-[40px] rounded-2xl bg-[#FFEA00] font-chaletLondon text-lg mt-5">Login</button>
                     </form>
-                    <p className="font-pricedown absolute text-8xl top-0 -mt-32 text-[#FF6A00]">Paleto</p>
+                    <p className="font-pricedown absolute text-8xl top-0 -mt-32 text-[#F55600]">Paleto</p>
                 </div>
             </div>
             <div className="flex-1 h-full w-full">
