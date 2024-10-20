a2c981d9d3bce026c292c544e532359b0a4ce7dc
rahuldhanak11 <rahuldhanak11@gmail.com>
2024-10-20 09:57:26 +0530

    font size change

--- Frontend/src/components/FlagsCard.jsx
+++ Frontend/src/components/FlagsCard.jsx
@@ -4,8 +4,8 @@ function FlagsCard() {
   return (
     <div className='p-8 '>
       <div className='relative'>
-          <h2 className='text-[#FFCC00] text-4xl font-pricedown'>Flags</h2>
-          <p className='text-white font-signPainter absolute left-16 -bottom-6 text-2xl'>Heyoo!</p>
+          <h2 className='text-[#FFCC00] text-6xl font-pricedown'>Flags</h2>
+          <p className='text-white font-signPainter absolute left-28 -bottom-6 text-2xl'>Ah! Here We Go Again</p>
         </div>
     </div>
   )
